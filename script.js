5a17bc82d53bb092ef09692ce100eb382a680a99
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-12-23 13:55:28 +0000

    Add functionality to display wrong letter and play again

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
 
-const figureParts = document.querySelectorAll('figure-part');
+const figureParts = document.querySelectorAll('.figure-part');
 
 const words = ['application', 'programming', 'interface', 'wizard'];
 
@@ -40,12 +40,29 @@ function showNotification() {
 
     setTimeout(() => {
         notification.classList.remove('show');
-
     }, 2000);
 }
 
 function updateWrongLettersEl() {
-    console.log('Update wrong')
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block'
+        } else {
+            part.style.display = 'none'
+        }
+    })
+
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunatelly you lost. 😕';
+        popup.style.display = 'flex';
+    }
 }
 
 window.addEventListener('keydown', e => {
@@ -69,6 +86,18 @@ window.addEventListener('keydown', e => {
 
         }
     }
+})
+
+playAgainBtn.addEventListener('click', () => {
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLettersEl();
+    popup.style.display = 'none';
 })
 
 displayWord();
